45dc75a856264f154b7c7627b079bc2b94e44532
Santheepkumar <devsantheep@gmail.com>
2021-10-15 16:09:12 +0530

    Brands slider added

---
+++ src/components/Brands/BrandsSlider.js
@@ -0,0 +1,107 @@
+// Import Swiper React components
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import "./BrandsSlider.scss";
+
+import "swiper/swiper-bundle.css";
+
+// swiper core styles
+// import "swiper/css";
+
+// modules styles
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+const Image =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQursqsAFp0EWMoWzrZ6u_V02GuFYhC3p2Q&usqp=CAU";
+const BrandsSlider = () => {
+  SwiperCore.use([Navigation]);
+
+  return (
+    <div className='Brands'>
+      <div className='Brands-Top'>
+        <h1 className='Brands-Top-Name'>Brands</h1>
+        <div className='Brands-Top-Img'>
+          <svg
+            className='prev Brands-Top-Img-Left'
+            xmlns='http://www.w3.org/2000/svg'
+            width='31'
+            height='20'
+            viewBox='0 0 31 20'>
+            <path
+              id='Left_arrow'
+              data-name='Left arrow'
+              className='cls-1'
+              d='M1456,2584h28a1,1,0,0,1,0,2h-28A1,1,0,0,1,1456,2584Zm7.32-8.71-9.03,9.03a0.983,0.983,0,1,0,1.39,1.39l9.03-9.03A0.983,0.983,0,1,0,1463.32,2575.29Zm-7.64,9,9.03,9.03a0.983,0.983,0,1,1-1.39,1.39l-9.03-9.03A0.983,0.983,0,1,1,1455.68,2584.29Z'
+              transform='translate(-1454 -2575)'
+            />
+          </svg>
+
+          <svg
+            className='next Brands-Top-Img-Left'
+            xmlns='http://www.w3.org/2000/svg'
+            width='31'
+            height='20'
+            viewBox='0 0 31 20'>
+            <path
+              id='Right_arrow'
+              data-name='Right arrow'
+              className='cls-1'
+              d='M1505,2584h28a1,1,0,0,1,0,2h-28A1,1,0,0,1,1505,2584Zm20.68-8.71,9.03,9.03a0.983,0.983,0,1,1-1.39,1.39l-9.03-9.03A0.983,0.983,0,1,1,1525.68,2575.29Zm7.64,9-9.03,9.03a0.983,0.983,0,1,0,1.39,1.39l9.03-9.03A0.983,0.983,0,1,0,1533.32,2584.29Z'
+              transform='translate(-1504 -2575)'
+            />
+          </svg>
+        </div>
+      </div>
+
+      <Swiper
+        style={{}}
+        navigation={{
+          prevEl: ".prev",
+          nextEl: ".next",
+        }}
+        spaceBetween={50}
+        breakpoints={{
+          1000: {
+            slidesPerView: 4,
+          },
+          811: {
+            slidesPerView: 2,
+          },
+          10: {
+            slidesPerView: 1,
+          },
+        }}
+        // slidesPerView={4}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='Brands-ImgMain' src={Image} alt='' />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BrandsSlider;

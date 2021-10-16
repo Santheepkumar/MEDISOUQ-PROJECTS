82b7c981b21e59656ab02060350463089fd7c5e0
Santheepkumar <devsantheep@gmail.com>
2021-10-16 14:45:22 +0530

    Blog mobile view

--- src/components/HealthBlog/BlogCard.js
+++ src/components/HealthBlog/BlogCard.js
@@ -3,13 +3,13 @@ import "./BlogCard.scss";
 
 export default function BlogCard({ name, date, link, img }) {
   return (
-    <div class='HealthBlog'>
-      <div class='HealthBlog-Main'>
-        <img class='HealthBlog-Main-Img' src={img} alt='' />
-        <h1 class='HealthBlog-Main-Name'>{name}</h1>
-        <div class='HealthBlog-Main-Bottom'>
-          <p class='HealthBlog-Main-Bottom-Left'>{date} </p>
-          <a class='HealthBlog-Main-Bottom-Right' href='/'>
+    <div class='HealthBlogCard'>
+      <div class='HealthBlogCard-Main'>
+        <img class='HealthBlogCard-Main-Img' src={img} alt='' />
+        <h1 class='HealthBlogCard-Main-Name'>{name}</h1>
+        <div class='HealthBlogCard-Main-Bottom'>
+          <p class='HealthBlogCard-Main-Bottom-Left'>{date} </p>
+          <a class='HealthBlogCard-Main-Bottom-Right' href='/'>
             {link}
           </a>
         </div>
